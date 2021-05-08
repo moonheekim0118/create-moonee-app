c4347250e232348b243a14a29a897d07eef822c8
moonee <hellomooneekim@gmail.com>
2021-05-09 01:08:24 +0900

    feat : add typescript-react template

---
+++ lib/templates/ts-react/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import RouteRoot from 'routes';
+class App extends Component{
+    render(){
+        return(
+            <div className="App">
+                 <RouteRoot />
+            </div>
+        )
+    }
+};
+
+export default App;
---
+++ lib/templates/ts-react/src/routes/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from 'pages/Home';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+const Root = ():JSX.Element=> {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Redirect path="*" to="/" />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Root;
