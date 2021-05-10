e0851e87bf7a18aaf9915f71469d46e7c7c03fab
moonee <hellomooneekim@gmail.com>
2021-05-10 10:19:54 +0900

    feat : handling unexpected Error

--- src/cli.js
+++ src/cli.js
@@ -8,64 +8,84 @@ const term = terminalKit.terminal;
 const fsp = fs.promises;
 
 const createDirectory = async (selectedOption, destDirName) => {
-  if (!selectedOption) return;
-  if (selectedOption.type === TYPES.QUIT) return TYPES.QUIT;
+  try {
+    if (!selectedOption) return;
+    if (selectedOption.type === TYPES.QUIT) return TYPES.QUIT;
 
-  const targetDirectionName = `../lib/templates/${selectedOption.templateName}`;
-  const targetDirPath = path.join(__dirname, targetDirectionName);
-  const destDirPath = path.join(process.cwd(), destDirName);
+    const targetDirectionName = `../lib/templates/${selectedOption.templateName}`;
+    const targetDirPath = path.join(__dirname, targetDirectionName);
+    const destDirPath = path.join(process.cwd(), destDirName);
 
-  const isExistDestDirPath = fs.existsSync(destDirPath);
-  term.spinner();
-  if (destDirName !== "." && isExistDestDirPath) return TYPES.EXIST_DEST;
-  if (destDirName === ".") {
-    const datas = await fsp.readdir(destDirPath);
-    if (datas.length > 0) return TYPES.EXIST_TARGET;
+    const isExistDestDirPath = fs.existsSync(destDirPath);
+    if (destDirName !== "." && isExistDestDirPath) return TYPES.EXIST_DEST;
+    if (destDirName === ".") {
+      const datas = await fsp.readdir(destDirPath);
+      if (datas.length > 0) return TYPES.EXIST_TARGET;
+    }
+
+    term.spinner();
+    await fse.copy(targetDirPath, destDirPath);
+    return selectedOption.type;
+  } catch (error) {
+    term.red(error);
+    return process.exit(0);
   }
-
-  await fse.copy(targetDirPath, destDirPath);
-  return selectedOption.type;
 };
 
 const getOption = async (descriptions) => {
-  return await term.singleColumnMenu(descriptions, {
-    style: term.green,
-    selectedStyle: term.bold.black.bgYellow,
-  }).promise;
+  try {
+    return await term.singleColumnMenu(descriptions, {
+      style: term.green,
+      selectedStyle: term.bold.black.bgYellow,
+    }).promise;
+  } catch (error) {
+    term.red(error);
+    return process.exit(0);
+  }
 };
 
 const getDestDirName = async () => {
-  const input = await term.inputField({}).promise;
-  if (!input) return defaultDirection;
-  else return input;
+  try {
+    const input = await term.inputField({}).promise;
+    if (!input) return defaultDirection;
+    else return input;
+  } catch (error) {
+    term.red(error);
+    return process.exit(0);
+  }
 };
 
 const startCommandLine = async (OptionsMap) => {
-  const optionsValue = [...OptionsMap.values()];
-  const descriptions = optionsValue.map((option) => option.description);
+  try {
+    const optionsValue = [...OptionsMap.values()];
+    const descriptions = optionsValue.map((option) => option.description);
 
-  term.cyan(CLI_MESSAGES.PRJOECT_NAME_QUESTION);
+    term.cyan(CLI_MESSAGES.PRJOECT_NAME_QUESTION);
 
-  const selectedDir = await getDestDirName();
-  const selectedType = await getOption(descriptions);
-  const selectedOption = OptionsMap.get(selectedType.selectedIndex);
-  const resultType = await createDirectory(selectedOption, selectedDir);
-  switch (resultType) {
-    case TYPES.CREATE:
-      term.cyan(CLI_MESSAGES.SUCCESS_MESSAGE);
-      return process.exit(0);
-    case TYPES.QUIT:
-      term.white(CLI_MESSAGES.QUIT_MESSAGE);
-      return process.exit(0);
-    case TYPES.EXIST_DEST:
-      term.red(CLI_MESSAGES.EXISTED_DEST_ERROR);
-      return startCommandLine(OptionsMap);
-    case TYPES.EXIST_TARGET:
-      term.red(CLI_MESSAGES.EXISTED_TARGET_ERROR);
-      return startCommandLine(OptionsMap);
-    default:
-      term.red(CLI_MESSAGES.FAILURE_MESSAGE);
-      return process.exit(0);
+    const selectedDir = await getDestDirName();
+    const selectedType = await getOption(descriptions);
+    const selectedOption = OptionsMap.get(selectedType.selectedIndex);
+    const resultType = await createDirectory(selectedOption, selectedDir);
+    switch (resultType) {
+      case TYPES.CREATE:
+        term.cyan(CLI_MESSAGES.SUCCESS_MESSAGE);
+        return process.exit(0);
+      case TYPES.QUIT:
+        term.white(CLI_MESSAGES.QUIT_MESSAGE);
+        return process.exit(0);
+      case TYPES.EXIST_DEST:
+        term.red(CLI_MESSAGES.EXISTED_DEST_ERROR);
+        return startCommandLine(OptionsMap);
+      case TYPES.EXIST_TARGET:
+        term.red(CLI_MESSAGES.EXISTED_TARGET_ERROR);
+        return startCommandLine(OptionsMap);
+      default:
+        term.red(CLI_MESSAGES.FAILURE_MESSAGE);
+        return process.exit(0);
+    }
+  } catch (error) {
+    term.red(error);
+    return process.exit(0);
   }
 };
 
