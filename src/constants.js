9bf59af68ddfdc0966318d8a2bd7013a89f092e6
moonee <hellomooneekim@gmail.com>
2021-05-10 10:24:15 +0900

    chore : update message

--- src/constants.js
+++ src/constants.js
@@ -2,11 +2,11 @@ export const defaultDirection = "new-app";
 
 export const CLI_MESSAGES = {
   PRJOECT_NAME_QUESTION: "Enter Your Project Name or Default 'new-app' ",
-  SELECT_OPTION_QUESTION: "Select Option",
+  SELECT_OPTION_QUESTION: "Select boilerplate to install",
   SUCCESS_MESSAGE:
-    "Your boilerplate is Successfully created! it's time to hack!",
+    "Your boilerplate is Successfully created! it's time to hack!	(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧",
   FAILURE_MESSAGE: "We can't create bolierplate now. Please try again\n",
-  QUIT_MESSAGE: "Bye!",
+  QUIT_MESSAGE: "Bye!(・∀・)ノ",
   EXISTED_DEST_ERROR:
     "Your selected Directory is already existed. Please select other directory\n",
   EXISTED_TARGET_ERROR:
@@ -14,17 +14,8 @@ export const CLI_MESSAGES = {
 };
 
 export const TYPES = {
-  CREATE: "create",
-  QUIT: "quit",
+  CREATE: "CREATE",
+  QUIT: "QUIT",
   EXIST_DEST: "EXIST_DEST",
   EXIST_TARGET: "EXIST_TARGET",
 };
-
-export const LOADING_LIST = [
-  "Installing React...",
-  "Configure Babel and Webpack...",
-  "Configure ESLint and Prettier...",
-  "Add some simple source...",
-  "Add some routing...",
-  "It's almost done...",
-];
