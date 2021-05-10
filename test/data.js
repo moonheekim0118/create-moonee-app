ca33cd1582ee2f222add8a237538e585e54afe76
moonee <hellomooneekim@gmail.com>
2021-05-11 01:52:40 +0900

    feat : add unit test for utils

---
+++ test/data.js
@@ -0,0 +1,35 @@
+exports.values = [
+  {
+    type: "CREATE",
+    defaultName: "React-JavaScript Bolierplate",
+    templateName: "js-react",
+    description: "React Bolierplate with JavaScript",
+  },
+  {
+    type: "CREATE",
+    defaultName: "React-TypeScript Bolierplate",
+    templateName: "ts-react",
+    description: "React Bolierplate with TypeScript",
+  },
+  {
+    type: "CREATE",
+    defaultName: "Next.js-JavaScript Bolierplate",
+    templateName: "js-next",
+    description: "Next.js Bolierplate with JavaScript",
+  },
+  {
+    type: "CREATE",
+    defaultName: "Next.js-TypeScript Bolierplate",
+    templateName: "ts-next",
+    description: "Next.js Bolierplate with TypeScript",
+  },
+  { type: "QUIT", description: "quit all the program" },
+];
+
+exports.descriptions = [
+  "React Bolierplate with JavaScript",
+  "React Bolierplate with TypeScript",
+  "Next.js Bolierplate with JavaScript",
+  "Next.js Bolierplate with TypeScript",
+  "quit all the program",
+];
---
+++ test/test.js
@@ -0,0 +1,56 @@
+// CLI 함수들 testing
+var _functions = require("../dist/utils");
+var should = require("chai").should();
+var cliOptions = require("../dist/options").default;
+var _datas = require("./data");
+
+describe("Validate PathName", function () {
+  const tests = [
+    { args: " ", expected: true },
+    { args: "*", expected: true },
+    { args: "<>", expected: true },
+    { args: "?", expected: true },
+    { args: ":", expected: true },
+    { args: "hi hi", expected: false },
+    { args: ".", expected: false },
+    { args: "123", expected: false },
+    { args: "!!", expected: false },
+    { args: "한글", expected: false },
+  ];
+  tests.forEach(({ args, expected }) => {
+    it(`${args}should return ${expected}`, function () {
+      const res = _functions.validatePathName(args);
+      res.should.equal(expected);
+    });
+  });
+});
+
+describe("Array To Map", () => {
+  it("should be correct Map", function () {
+    const res = _functions.arrayToMap(cliOptions);
+    const typeCheck = res instanceof Map;
+    typeCheck.should.equal(true);
+  });
+});
+
+describe("Extract Map Values", () => {
+  const res = _functions.arrayToMap(cliOptions);
+});
+
+describe("Map Related Functions", () => {
+  const map = _functions.arrayToMap(cliOptions);
+  const valuesArray = _functions.extractMapValues(map);
+  const descriptionOnly = _functions.extractDescription(valuesArray);
+
+  it("should be correct Map", function () {
+    const typeCheck = map instanceof Map;
+    typeCheck.should.equal(true);
+  });
+
+  it("shoud be correct Array", function () {
+    valuesArray.should.deep.equal(_datas.values);
+  });
+  it("should be correct description array", function () {
+    descriptionOnly.should.deep.equal(_datas.descriptions);
+  });
+});
