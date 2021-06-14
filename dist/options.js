079c77a4d42003a90cef27495cebf44dc0df21e9
moonee <hellomooneekim@gmail.com>
2021-06-14 16:01:03 +0900

    fix typos & config

--- dist/options.js
+++ dist/options.js
@@ -1,35 +1,41 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+  value: true,
 });
 exports.default = void 0;
 
 var _constants = require("./constants");
 
-const cliOptions = [{
-  type: _constants.TYPES.CREATE,
-  defaultName: "React-JavaScript Bolierplate",
-  templateName: "js-react",
-  description: "React Bolierplate with JavaScript"
-}, {
-  type: _constants.TYPES.CREATE,
-  defaultName: "React-TypeScript Bolierplate",
-  templateName: "ts-react",
-  description: "React Bolierplate with TypeScript"
-}, {
-  type: _constants.TYPES.CREATE,
-  defaultName: "Next.js-JavaScript Bolierplate",
-  templateName: "js-next",
-  description: "Next.js Bolierplate with JavaScript"
-}, {
-  type: _constants.TYPES.CREATE,
-  defaultName: "Next.js-TypeScript Bolierplate",
-  templateName: "ts-next",
-  description: "Next.js Bolierplate with TypeScript"
-}, {
-  type: _constants.TYPES.QUIT,
-  description: "quit all the program"
-}];
+const cliOptions = [
+  {
+    type: _constants.TYPES.CREATE,
+    defaultName: "React-JavaScript Bolierplate",
+    templateName: "js-react",
+    description: "React boilerplate with JavaScript",
+  },
+  {
+    type: _constants.TYPES.CREATE,
+    defaultName: "React-TypeScript Bolierplate",
+    templateName: "ts-react",
+    description: "React boilerplate with TypeScript",
+  },
+  {
+    type: _constants.TYPES.CREATE,
+    defaultName: "Next.js-JavaScript boilerplate",
+    templateName: "js-next",
+    description: "Next.js boilerplate with JavaScript",
+  },
+  {
+    type: _constants.TYPES.CREATE,
+    defaultName: "Next.js-TypeScript boilerplate",
+    templateName: "ts-next",
+    description: "Next.js boilerplate with TypeScript",
+  },
+  {
+    type: _constants.TYPES.QUIT,
+    description: "quit all the program",
+  },
+];
 var _default = cliOptions;
 exports.default = _default;
